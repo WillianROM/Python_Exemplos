58a6e3dec9eeaf2605bdddfd6e3cc57c05b19ab8
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2023-01-14 14:54:40 -0300

    Update meu_robo_notepad.py

--- Robos_Python/meu_robo_notepad.py
+++ Robos_Python/meu_robo_notepad.py
@@ -5,13 +5,13 @@ from pywinauto.application import Application
 app = Application(backend='uia').start(r'notepad.exe') # Para abrir o aplicativo
 app = Application(backend='uia').connect(title='Sem título - Bloco de Notas', timeout=20) # Para conectar o aplicativo
 
-window = app.SemTituloBlocoDeNotas
+window = app.SemTituloBlocoDeNotas # Lembrando que depois do app. vem o nome do título do aplicativo sem espaços e traços
 window.set_focus()
 
-#window.print_control_identifiers() # Para ver os identificadores dos elementos do aplicativo (Lembrando que depois do app. vem o nome do título do aplicativo sem espaços e traços)
+#window.print_control_identifiers() # Para ver os identificadores dos elementos do aplicativo 
 
 textEditor = window.child_window(title="Editor de Texto", auto_id="15", control_type="Edit").wrapper_object()
-textEditor.type_keys("Hello, World! How are you, guys? João e José, vocês são legais.", with_spaces = True) # É necessário colocar with_spaces = True para ser digitado os espaços
+textEditor.type_keys("Hello, World! How are you, guys?", with_spaces = True) # É necessário colocar with_spaces = True para ser digitado os espaços
 
 """ fileMenu = window.child_window(title="Arquivo", control_type="MenuItem").wrapper_object()
 fileMenu.click_input() # Clique usando o mouse, tem ótima funcionalidade
