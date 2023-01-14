62d1886f0e8232dcfd2578224aca8575592d4b3b
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2023-01-14 17:09:12 -0300

    Update ApiCreateExecution.py

--- ApisRequests/ApiCreateExecution.py
+++ ApisRequests/ApiCreateExecution.py
@@ -2,5 +2,10 @@ import requests # pip install requests
 
 requisicao = requests.get('https://minhaprimeiraapi.willianmelo1.repl.co/pegarvendas')
 
+dicionario = requisicao.json()
+
 print(requisicao) # Para ver o status da requisição, por exemplo 200
-print(requisicao.json()) # Para ver o corpo do response em formato json
+print(dicionario) # Para ver o corpo do response em formato json
+
+print(dicionario['total vendas'])
+
