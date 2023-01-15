739caad57e802248cdbff70f6e177fbbf6b6ae83
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2023-01-15 15:39:04 -0300

    Update quickstart.py

--- GoogleSheets/quickstart.py
+++ GoogleSheets/quickstart.py
@@ -14,17 +14,14 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 # The ID and range of a sample spreadsheet.
 import idPlanilhaTeste
 SAMPLE_SPREADSHEET_ID = idPlanilhaTeste.id_Planilha # Id da planilha que será manipulado, deixei o id dessa planilha em outro arquivo py chamado idPlanilhaTeste que não vai ficar disponível no meu gitHub
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+SAMPLE_RANGE_NAME = 'abaTeste!A1:B14'
 
 
 def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+
+    # Essa parte é para ter acesso, se houver não houver token válido, utilizará as credenciais do arquivo json para gerar um nono token:
+
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
@@ -39,23 +36,63 @@ def main():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
+# Essa parte onde começa a fazer as manipulações do arquivo google sheets
+
     try:
-        service = build('sheets', 'v4', credentials=creds)
+        service = build('sheets', 'v4', credentials=creds) # Para criar um serviço
 
         # Call the Sheets API
-        sheet = service.spreadsheets()
+        sheet = service.spreadsheets() # Definir um arquivo
+
+        # result = sheet.values().get para let informações da planilha
+        # result = sheet.values().update para adicionar/editar informações da planilha
+        # https://developers.google.com/sheets/api/guides/values#python
+
+        #GET
         result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                     range=SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
+        valores = result['values']
+        print(valores)
 
-        if not values:
-            print('No data found.')
-            return
+        #UPDATE
+        valores_adicionar = [
+            ["dezembro", 'R$ 99.555,33'],
+            ['janeiro', 'R$ 66.555,22']
+        ]
 
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
+
+        result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range="A13", valueInputOption="USER_ENTERED",
+                                    body={'values': valores_adicionar}).execute()
+                                    # RAW - The input is not parsed and is simply inserted as a string, so the input "=1+2" places the string "=1+2" in the cell, not a formula. (Non-string values like booleans or numbers are always handled as RAW.)
+                                    # USER_ENTERED - The input is parsed exactly as if it were entered into the Google Sheets UI, so "Mar 1 2016" becomes a date, and "=1+2" becomes a formula. Formats may also be inferred, so "$100.15" becomes a number with currency formatting.
+
+    # Adicionar uma nova coluna com os valores dos impostos
+        # Pegar os valores atuais
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        valores = result['values']
+
+        # Criar uma lista com o título da nova coluna
+        valores_adicionar = [
+            ["imposto"]
+        ]
+
+        # Cálcular os valores dos impostos por mês, transformando os valores das vendas em float e cálculando 10% e adicionando os valores dos impostos na lista criado anteriormente
+        for i, linha in enumerate(valores): #Para pegar o índice
+            if i > 0:
+                vendas = linha [1]
+                vendas = vendas.replace("R$ ","").replace(".","").replace(",",".")
+                vendas = float(vendas) #Converter a String para Float
+                imposto = vendas * 0.1
+                valores_adicionar.append([imposto]) # Para adicionar itens a lista, lembrando que a variável deve estar entre colchetes []
+
+        # Adicionar a nova coluna na planilha a partir da célula C1
+        result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range="C1", valueInputOption="USER_ENTERED",
+                                    body={'values': valores_adicionar}).execute()
+
+
     except HttpError as err:
         print(err)
 
