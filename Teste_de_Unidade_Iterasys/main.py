5338720275c5efeac5ab7092338c793f1864ed88
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2023-01-15 13:42:53 -0300

    Create main.py

---
+++ Teste_de_Unidade_Iterasys/main.py
@@ -0,0 +1,20 @@
+# Bibliotecas
+    #Não usado nesse projeto
+
+
+# Funções recorrentes ou de apoio
+    #  Calcular a soma de 2 número
+def somar_dois_numeros(numeroA, numeroB):
+    return numeroA + numeroB
+
+
+# Inicialização
+if __name__ == '__main__':  # Gatilho para iniciar o programa
+    numeroA = 5
+    numeroB = 2
+    resultado = somar_dois_numeros(numeroA, numeroB)
+    print('A soma de {:.2f} com {:.2f} é igual a {:.2f}'.format(numeroA, numeroB, resultado))
+
+
+# Funções gerais
+    #Não usado nesse projeto
